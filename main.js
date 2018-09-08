a43a49a95b2ac271a75827472c5eedec9dd08e18
Elle H <itsellej@gmail.com>
2018-09-08 19:28:28 +0100

    Corrected last if statement in coinflip function

--- main.js
+++ main.js
@@ -3,14 +3,27 @@ const coinflip = (num) => {
   let tails = 0
   let heads = 0
 
-  for(let i = 0; i <= num; i++){
+  for(let i = 0; i < num; i++){
     let randomNumber = Math.floor(Math.random() * 2);
+    console.log("random " + randomNumber);
 
-    if(randomNumber = 0){
+    if(randomNumber === 0){
       heads++;
+      console.log("heads:" + heads)
     }
     else{
-      tails++
+      tails++;
+      console.log("tails:" + tails)
     }
   }
-}
+
+  if(heads > tails){
+    return "heads wins: " + heads + " flips"
+  }
+  else if (tails > heads){
+    return "tails wins: " + tails + " flips";
+  }
+  else{
+    return "it's a tie!"
+  }
+};
