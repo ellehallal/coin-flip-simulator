025fe5f67d5a8cc99634b111906395715ff6ae78
Elle H <itsellej@gmail.com>
2018-09-08 21:06:29 +0100

    Add min and max parameters to number input. Add warning message for numbers outside of range

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let number = document.getElementById("number");
+let alert = document.getElementById("alert");
 let button = document.getElementById("flipbutton");
 let results = document.getElementById("results");
 let headsresults = document.getElementById("headsresults");
@@ -8,35 +9,39 @@ button.addEventListener("click", function() {
   let tails = 0;
   let heads = 0;
   let usernumber = parseInt(number.value);
-  console.log(usernumber)
+  console.log(usernumber);
 
-  for (let i = 0; i < usernumber; i++) {
-    let randomNumber = Math.floor(Math.random() * 2);
-    console.log("random " + randomNumber);
-
-    if (randomNumber === 0){
-      heads++;
-      console.log("heads:" + heads);
-    }
-    else {
-      tails++;
-      console.log("tails:" + tails);
-    }
+  if (usernumber < 1 || usernumber > 1000) {
+    alert.innerHTML = "Please enter a number between 1 - 1000";
+    setTimeout(function () { alert.innerHTML = "";}, 1200);
   }
 
-  if(heads > tails){
-    results.innerHTML =  "heads wins: " + heads + " flips";
-  }
-  else if (tails > heads){
-    results.innerHTML = "tails wins: " + tails + " flips";
-  }
   else {
-    results.innerHTML = "it's a tie!";
-  }
+    for (let i = 0; i < usernumber; i++) {
+      let randomNumber = Math.floor(Math.random() * 2);
+      console.log("random " + randomNumber);
 
-  headsresults.innerHTML = "Heads: " + heads;
-  tailsresults.innerHTML = "Tails: " + tails;
-  console.log(usernumber)
+      if (randomNumber === 0) {
+        heads++;
+        console.log("heads:" + heads);
+      } else {
+        tails++;
+        console.log("tails:" + tails);
+      }
+    }
+
+    if (heads > tails) {
+      results.innerHTML = "Heads wins with " + heads + " flips!";
+    } else if (tails > heads) {
+      results.innerHTML = "Tails wins with " + tails + " flips!";
+    } else {
+      results.innerHTML = "It's a tie!";
+    }
+
+    headsresults.innerHTML = "Heads: " + heads;
+    tailsresults.innerHTML = "Tails: " + tails;
+    console.log(usernumber)
+  }
 });
 
 // button.addEventListener("click", coinflip());
